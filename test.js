b65935521a9e5f4e88768b71a498802c8d1e78b5
root <root@DESKTOP-55QGKKE.localdomain>
2022-05-11 10:37:57 +0900

    #1 klaytn version, blockNumber code

---
+++ test.js
@@ -0,0 +1,10 @@
+const Caver = require('caver-js');
+const caver = new Caver('https://api.baobab.klaytn.net:8651/');
+
+
+
+caver.klay.getBlockNumber(function(err, blockNumber) {
+    console.log(blockNumber);
+});
+
+
